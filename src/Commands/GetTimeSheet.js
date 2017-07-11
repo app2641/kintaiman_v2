8bc213c1221e27b7b056230cca88d6c327d064f4
app2641 <app2641@gmail.com>
2017-07-11 17:13:51 +0900

    Reply even when there is no timesheet

--- src/Commands/GetTimeSheet.js
+++ src/Commands/GetTimeSheet.js
@@ -4,15 +4,16 @@ export default class GetTimeSheet extends AbstractTimeSheetCommand {
   run() {
     const month = this.parseMonth(this.text);
     const timeSheetId = this.settings.getTimeSheetId(month);
-    if (!timeSheetId) return;
-
-    this.timeSheetUrl = `https://docs.google.com/spreadsheets/d/${timeSheetId}/edit`;
+    if (timeSheetId) this.timeSheetUrl = `https://docs.google.com/spreadsheets/d/${timeSheetId}/edit`;
 
     return true;
   }
 
   buildMessage() {
-    if (!this.timeSheetUrl) return;
-    return `@${this.userName} ${this.timeSheetUrl}`;
+    if (this.timeSheetUrl) {
+      return `@${this.userName} ${this.timeSheetUrl}`;
+    }
+
+    return `@${this.userName} 日報が見つかりませんでした`;
   }
 }
--- test/Commands/GetTimeSheet_test.js
+++ test/Commands/GetTimeSheet_test.js
@@ -5,18 +5,31 @@ import Settings from '../../src/Settings';
 describe('GetTimeSheet', () => {
   const settings = new Settings();
   const userName = 'app2641';
-  const text = '1月の日報をくれ';
-  const command = new GetTimeSheet(settings, userName, text);
 
   describe('run', () => {
+    const text = '1月の日報をくれ';
+    const command = new GetTimeSheet(settings, userName, text);
+
     it('get time sheet', () => {
       assert.equal(command.run(), true);
     });
   });
 
   describe('buildMessage', () => {
-    it('message for post', () => {
+    it('found timesheet', () => {
+      const text = '1月の日報をくれ';
+      const command = new GetTimeSheet(settings, userName, text);
+      command.run();
+
       assert.equal(command.buildMessage(), '@app2641 https://docs.google.com/spreadsheets/d/spreadsheetId/edit');
+    });
+
+    it('not found timesheet', () => {
+      const text = '10月の日報をくれ';
+      const command = new GetTimeSheet(settings, userName, text);
+      command.run();
+
+      assert.equal(command.buildMessage(), '@app2641 日報が見つかりませんでした');
     });
   });
 });
